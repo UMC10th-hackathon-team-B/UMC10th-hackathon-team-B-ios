63131f7c04ea4a127e0b7adcd1900be6f2f88f8e
junyear <kimjunhyeon6@gmail.com>
2026-06-16 18:31:59 +0900

    🔧 Fix submodule issue

---
+++ EggWatch/EggWatch/EggWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggWatchApp.swift
+//  EggWatch
+//
+//  Created by JOON on 6/16/26.
+//
+
+import SwiftUI
+
+@main
+struct EggWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

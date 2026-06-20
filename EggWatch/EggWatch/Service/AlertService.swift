ce5872ab245a4cf362fa6455aeafd32e37131763
choiyunseok0 <choiyunseok128@naver.com>
2026-06-20 20:59:39 +0900

    [Feat] AlertService 기본 구조 작성

--- EggWatch/EggWatch/Service/AlertService.swift
+++ EggWatch/EggWatch/Service/AlertService.swift
@@ -4,4 +4,19 @@
 //
 //  Created by JOON on 6/17/26.
 //
+//  알림함 조회/읽음 처리 API를 호출하는 서비스
 
+import Foundation
+import Moya
+
+// MARK: - 알림 서비스
+class AlertService {
+
+    // MARK: - Moya Provider
+    // AuthPlugin: accessToken 헤더 자동 첨부
+    // TokenRefreshPlugin: 401 시 자동 재발급
+    private let provider = MoyaProvider<NotificationRouter>(plugins: [
+        AuthPlugin(),
+        TokenRefreshPlugin()
+    ])
+}
